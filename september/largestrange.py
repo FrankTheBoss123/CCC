b690d03410c51d78792c7d72c8fc5de5588e109a
FrankTheBoss123 <46289416+FrankTheBoss123@users.noreply.github.com>
2020-09-06 11:25:08 -0700

    tried to do largest range but failed

---
+++ september/largestrange.py
@@ -0,0 +1,13 @@
+
+def largestrange(array):
+    array = array.sort()
+    index1 = 0
+    index2 = 0
+    biggest = 0
+    biggest_index1, biggest_index2 = 0,0
+    for x in range(0,len(array)):
+        if x != len(array):
+            if array[x]==array[x+1]+1
+                if index1!=x
+            else:
+        else:
