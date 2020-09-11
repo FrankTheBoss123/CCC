a5a3367b957891b23af5061360b6a10977bee7cd
FrankTheBoss123 <46289416+FrankTheBoss123@users.noreply.github.com>
2020-09-10 17:51:31 -0700

    finished the question

--- september/subarraysort.py
+++ september/subarraysort.py
@@ -1,1 +1,15 @@
 #i don't know how to do this question
+def subarraysort(array):
+    start = -1
+    end = -1
+    for x in range(len(array)):
+        if array[x]!=min(array[x:]):
+            start = x
+            break
+    for x in range(len(array)-1,-1,-1):
+        if array[x]!=max(array[:x+1]):
+            end = x
+            break
+    return [start,end]
+
+print(subarraysort([1,2,3,7,10,11,7,12,6,7,16,18,19]))
